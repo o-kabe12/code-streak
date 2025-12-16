4fca90ce0f389d92d4187dd8ecc951473ae45e74
okabe <koo710128@gmail.com>
2025-12-16 22:25:16 +0900

    #1 ダミーのTOPページ作成

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
+      <Header />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="w-[90%] mx-auto">
+          {children}
+        </div>
       </body>
     </html>
   );
---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="w-full bg-white shadow-md">
+      <div className="w-[90%] mx-auto py-4 flex justify-between items-center">
+        <h1 className="text-3xl font-bold">
+          <Link href="/">Code Streak</Link>
+        </h1>
+      </div>
+    </header>
+  );
+}
